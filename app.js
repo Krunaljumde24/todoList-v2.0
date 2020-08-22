cdbe0b370cdf7700362ce91753b7aa41fe80288d
Krunal Jumde <krunaljumde24@gmail.com>
2020-08-22 13:16:23 +0530

    changning the port to heroku dynamic port

--- app.js
+++ app.js
@@ -128,10 +128,13 @@ List.findOne({name: customListName} , (err,foundList)=>{
 });
 });
 
+let port = process.env.port
+if(port === null || port === ""){
+  port = 3000;
+}
 
 
-
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
 
